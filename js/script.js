005c82078eb1c36fe3997b15e1ff5046a98a2b91
Mohammad Zaid <mhdzaid9120@gmail.com>
2024-04-17 19:34:39 +0530

    Major Updates
    Projects, Timeline, Contacts, Content and few CSS updates.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*============ typing animation =============*/ 
 var typed = new Typed(".typing", {
-    strings:["","Web Designer", "Web Developer", "Graphic Designer", "Youtuber"],
+    strings:["","Web Designer","UI/UX Designer", "Web Developer", "Graphic Designer", "Youtuber"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
